c56396a344c348c6659761100ac855c106684faf
Anduin Xue <anduin@aiursoft.com>
2024-12-21 14:44:39 +0000

    Update CheckRecentUpdateTime.psm1

--- tools/CheckRecentUpdateTime.psm1
+++ tools/CheckRecentUpdateTime.psm1
@@ -1,8 +1,19 @@
 function CheckRecentUpdateTime {
-    $updateTimes = Get-HotFix | Select-Object -ExpandProperty InstalledOn
-    $mostRecentUpdateTime = $updateTimes | Sort-Object -Descending | Select-Object -First 1
-    $recentMonthInstalledAnUpdate = $mostRecentUpdateTime -gt (Get-Date).AddDays(-45)
-    return $recentMonthInstalledAnUpdate
+    # Get all hotfixes and filter out any that don't have a valid 'InstalledOn' date
+    $updateTimes = Get-HotFix | Where-Object { $_.InstalledOn -ne $null } | Select-Object -ExpandProperty InstalledOn
+
+    # Check if there are any valid dates, then find the most recent one
+    if ($updateTimes.Count -gt 0) {
+        $mostRecentUpdateTime = $updateTimes | Sort-Object -Descending | Select-Object -First 1
+        
+        # Check if the most recent update was within the last 45 days
+        $recentMonthInstalledAnUpdate = $mostRecentUpdateTime -gt (Get-Date).AddDays(-45)
+        
+        return $recentMonthInstalledAnUpdate
+    } else {
+        Write-Warning "No updates found with valid 'InstalledOn' dates."
+        return $false
+    }
 }
 
 Export-ModuleMember -Function CheckRecentUpdateTime
